b303cb4cbcdfe9c85f846ad0ef64b7971066a7b7
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-06-06 21:52:27 +0200

    Add class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -244,8 +244,8 @@
 
 
 
-      console.log('AmountWidget: ', thisWidget);
-      console.log('constructor argument: ', element);
+      //console.log('AmountWidget: ', thisWidget);
+      //console.log('constructor argument: ', element);
     }
     
     getElements(element){
@@ -273,7 +273,7 @@
       } if(thisWidget.value <= settings.amountWidget.defaultMin){
         thisWidget.value = settings.amountWidget.defaultMin;
       }
-      console.log('value: ', thisWidget.value);
+      //console.log('value: ', thisWidget.value);
       thisWidget.input.value = thisWidget.value;
       thisWidget.announce();
     }
@@ -302,6 +302,35 @@
 
     
   }
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('card: ', thisCart);
+
+    }
+
+    getElements(element){
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      console.log('thisCart.dom.togle: ', thisCart.dom.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+  
+
   
   const app = {
     initData: function(){
@@ -321,6 +350,11 @@
       
       
     },
+    initCart: function(){
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
 
     init: function(){
       const thisApp = this;
@@ -332,6 +366,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
